3d75fa96d898883fa3ec63786e253e3447af0e3c
Felipe Menanteau <felipe@illinois.edu>
2016-05-04 18:48:48 +0000

    Adding EXTNAME to xcorr catalog

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ bin_files = glob.glob("bin/compute_*") + glob.glob("bin/*.txt")
 
 # The main call
 setup(name='extinction',
-      version ='1.2.0',
+      version ='1.2.1',
       license = "GPL",
       description = "A Dust Extinction correction module for DESDM.",
       author = "Felipe Menanteau",
